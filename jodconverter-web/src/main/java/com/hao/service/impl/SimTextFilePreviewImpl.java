68b687ff0a65aa7e00ddc506c39e112d7b1d1c8b
hzw517262544 <517262544@qq.com>
2018-12-17 18:37:49 +0800

    filepreview

---
+++ jodconverter-web/src/main/java/com/hao/service/impl/SimTextFilePreviewImpl.java
@@ -0,0 +1,40 @@
+package com.hao.service.impl;
+
+import com.hao.model.FileAttribute;
+import com.hao.model.ReturnResponse;
+import com.hao.service.FilePreview;
+import com.hao.utils.FileUtils;
+import com.hao.utils.SimTextUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+/**
+ * Created by kl on 2018/1/17.
+ * Content :处理文本文件
+ */
+@Service
+public class SimTextFilePreviewImpl implements FilePreview{
+
+    @Autowired
+    SimTextUtil simTextUtil;
+
+    @Autowired
+    FileUtils fileUtils;
+
+    @Override
+    public String filePreviewHandle(String url, Model model){
+        FileAttribute fileAttribute=fileUtils.getFileAttribute(url);
+        String decodedUrl=fileAttribute.getDecodedUrl();
+        String fileName=fileAttribute.getName();
+        ReturnResponse<String> response = simTextUtil.readSimText(decodedUrl, fileName);
+        if (0 != response.getCode()) {
+            model.addAttribute("msg", response.getMsg());
+            model.addAttribute("fileType",fileAttribute.getSuffix());
+            return "fileNotSupported";
+        }
+        model.addAttribute("ordinaryUrl", response.getMsg());
+        return "txt";
+    }
+
+}
